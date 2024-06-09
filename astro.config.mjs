1b973ea2f1166de7633f073b5b77acc48a91307b
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2024-06-09 19:10:25 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: 'https://mo9a7i.github.io',
-  base: 'ts_astro_arabic_reverse_typing'
+  site: 'https://arabic-keyboard.mo9a7i.com',
+  base: '/'
 });
